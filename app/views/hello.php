fef1721a63c5341f31fbaed220214956dcedfa61
Skyler Knight <skyler.knight22@gmail.com>
2014-05-14 17:30:13 -0600

    add all files

---
+++ app/views/hello.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<link rel="stylesheet" href="stylesheets/bootstrap.yeti.css">
+  <link rel="stylesheet" href="stylesheets/main-styles.css">
+</head>
+<body>
+	<div id="main-nav" class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Skyer Knight</a>
+  </div>
+  <div class="navbar-collapse collapse navbar-responsive-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Active</a></li>
+      <li><a href="#">Link</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  </div>
+</div>
+<header id="main-header">
+  <h1 class="banner-title">SKYLER KNIGHT</h1>
+  <!-- <img src="img/header-image.jpg" alt=""> -->
+  <div id="profile-image"><img src="https://lh4.googleusercontent.com/-82MRltWXV0Q/UDwbWf_xNNI/AAAAAAAAAL0/bhGWJ-NAhiY/w445-h447-no/photo.JPG" alt=""></div>
+</header>
+<div id="main-content">
+  <br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br>
+
+</div>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
