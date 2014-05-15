1813e8680f2db9dcb446c90a6be9e9e1c1a33a19
theknightsky <skyler.knight22@gmail.com>
2014-05-14 22:41:12 -0600

    nav & content style updates

--- app/views/hello.php
+++ app/views/hello.php
@@ -8,31 +8,32 @@
 </head>
 <body>
 	<div id="main-nav" class="navbar navbar-default navbar-fixed-top">
-  <div class="container-fluid">
+  <div class="container">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="#">Skyer Knight</a>
+    <a class="navbar-brand" href="#">SK</a>
   </div>
   <div class="navbar-collapse collapse navbar-responsive-collapse">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Active</a></li>
-      <li><a href="#">Link</a></li>
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">What you'll learn</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Link</a></li>
       <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Language <b class="caret"></b></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
+          <li><a href="#">한국어어</a></li>
+          <li><a href="#">日本語</a></li>
         </ul>
+      </li>
+      <li class="navbar-form">
+        <!-- <div class="navbar-form"> -->
+          <button type="button" class="btn btn-danger navbar-btn">Book me!</button>
+        <!-- </div> -->
       </li>
     </ul>
   </div>
@@ -40,16 +41,45 @@
 </div>
 <header id="main-header">
   <h1 class="banner-title">SKYLER KNIGHT</h1>
-  <!-- <img src="img/header-image.jpg" alt=""> -->
-  <div id="profile-image"><img src="https://lh4.googleusercontent.com/-82MRltWXV0Q/UDwbWf_xNNI/AAAAAAAAAL0/bhGWJ-NAhiY/w445-h447-no/photo.JPG" alt=""></div>
+  <!-- <div id="profile-image"><img src="https://d39mbatqcgbp1v.cloudfront.net/photos/5522/small/skimaTalk.jpg?1399090582" alt=""></div> -->
 </header>
 <div id="main-content">
-  <br><br><br><br><br><br><br><br><br>
-  <br><br><br><br><br><br><br><br><br>
-  <br><br><br><br><br><br><br><br><br>
-  <br><br><br><br><br><br><br><br><br>
-  <br><br><br><br><br><br><br><br><br>
-
+  <section class="gray">
+    <h1 class="section-header">What you'll learn</h1>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="teaching-skill"><h4 class="red">Communication</h4>
+            <p>Get real practice with a native English speaker</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="teaching-skill"><h4 class="blue">Pronunciation</h4>
+            <p>Improve your pronunciation with real time corrections</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="teaching-skill"><h4 class="orange">Vocabulary</h4>
+            <p>Learn new vocabulary each and every lesson!</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="teaching-skill"><h4 class="green">Listening</h4>
+            <p>No more recordings... Practice listening with a native speaker</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="light-red">
+    <h1 class="section-header">Extensive Feedback</h1>
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+  </section>
+  <section>
+    <h1 class="section-header">My Philosophy</h1>
+  </section>
 </div>
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
